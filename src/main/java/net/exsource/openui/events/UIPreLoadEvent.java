6713f544eb4b87d11843521d11f0a4406265fe92
danielramke <daniel.ramke@lyth.de>
2023-06-29 15:17:43 +0200

    update dependency open-utils and change UIPreLoadEvent.

--- src/main/java/net/exsource/openui/events/UIPreLoadEvent.java
+++ src/main/java/net/exsource/openui/events/UIPreLoadEvent.java
@@ -1,13 +1,15 @@
 package net.exsource.openui.events;
 
+import net.exsource.openutils.event.Cancelable;
 import net.exsource.openutils.event.Event;
 import net.exsource.openutils.io.controller.PropertiesController;
 import org.jetbrains.annotations.NotNull;
 
-public class UIPreLoadEvent extends Event {
+public class UIPreLoadEvent implements Event, Cancelable {
 
     private final Class<?> mainClass;
     private final PropertiesController properties;
+    private boolean cancelled;
 
     public UIPreLoadEvent(@NotNull Class<?> mainClass, PropertiesController properties) {
         this.mainClass = mainClass;
@@ -24,5 +26,20 @@ public class UIPreLoadEvent extends Event {
 
     public PropertiesController getProperties() {
         return properties;
+    }
+
+    @Override
+    public String getName() {
+        return getClass().getSimpleName();
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
     }
 }
